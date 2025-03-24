e56bc058321ad26a5ecbe145b68aca9113a8267b
Zel <israel88andrade@gmail.com>
2025-03-24 15:27:10 -0300

    orphan

---
+++ src/main/java/com/zel92/udemy_java_course/UdemyJavaCourseApplication.java
@@ -0,0 +1,13 @@
+package com.zel92.udemy_java_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyJavaCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyJavaCourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zel92/udemy_java_course/UdemyJavaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zel92.udemy_java_course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyJavaCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
